d5f17b58bfe4441560b83dde275dccb89d612cca
HwangSunTae <pozafly@gmail.com>
2020-11-29 21:25:33 +0900

    라우터 네이게이션을 이용한 데이터 호출 방법

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import JobsView from '../views/JobsView.vue';
 import ItemView from '../views/ItemView.vue';
 import UserView from '../views/UserView.vue';
 // import createListView from '../views/CreateListView.js';
+import bus from '../utils/bus.js';
+import { store } from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -21,7 +23,20 @@ export const router = new VueRouter({
       path: '/news',
       name: 'news',
       // component: createListView('NewsView')
-      component: NewsView
+      component: NewsView,
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            console.log('fetched');
+            bus.$emit('end:spinner');
+            next();
+          })
+          .catch(e => console.log(e));
+        // console.log('to : ', to);
+        // console.log('from : ', from);
+        // console.log('next : ', next);
+      }
     },
     {
       path: '/ask',
