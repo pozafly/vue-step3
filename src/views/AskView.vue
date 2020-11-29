2bf8b89ee44af142aded52951e06aeff251bddf2
HwangSunTae <pozafly@gmail.com>
2020-11-29 16:05:27 +0900

    질문, 구직 리스트 스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <p v-for="item in ask" :key="item.title">
-      <router-link :to="`/item/${item.id}`">
-        {{ item.title }}
-      </router-link>
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in ask" :key="item.title" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link :to="`/item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by 
+            <router-link :to="`/user/${item.user}`" class="link-text">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -24,6 +40,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  padding: 0;
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
