db67244defe7ef44cc381cbdc3724d5235021d34
HwangSunTae <pozafly@gmail.com>
2020-11-29 16:21:10 +0900

    공통 컴포넌트 ListItem 제작 및 실습

---
+++ src/components/ListItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <ul class="news-list">
+      <li v-for="item in news" :key="item.title" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a :href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            by 
+            <router-link :to="`/user/${item.user}`" class="link-text">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      news: 'fetchedNews'
+    })
+  },
+  created() {
+    this.$store.dispatch('FETCH_NEWS');
+  }
+}
+</script>
+
+<style scoped>
+.news-list {
+  padding: 0;
+  margin: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
