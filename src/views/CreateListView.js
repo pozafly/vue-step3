dffed0b88256384113d9f86c7813e169e22cae17
HwangSunTae <pozafly@gmail.com>
2020-11-29 20:12:23 +0900

    하이 오더 컴포넌트가 적용된 앱 구조

--- src/views/CreateListView.js
+++ src/views/CreateListView.js
@@ -4,17 +4,15 @@ import bus from '../utils/bus.js';
 export default function createListView(name) {
   return {
     // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
-    name: name,
+    name,
     created() {
       bus.$emit('start:spinner');
-      setTimeout(() => {
-        this.$store.dispatch('FETCH_LIST', this.$route.name)
-          .then(() => {
-            console.log('fetched');
-            bus.$emit('end:spinner');
-          })
-          .catch(e => console.log(e));
-      }, 1000);
+      this.$store.dispatch('FETCH_LIST', this.$route.name)
+        .then(() => {
+          console.log('fetched');
+          bus.$emit('end:spinner');
+        })
+        .catch(e => console.log(e));
     },
     render(createElement) {
       return createElement(ListView);
