7601096aaacc047782f2a991ea001efd9721aa46
HwangSunTae <pozafly@gmail.com>
2020-11-29 17:09:43 +0900

    사용자 프로필 컴포넌트 데이터 흐름처리 1

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="user-container">
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <div>{{ userInfo.id }}</div>
+      <!-- <router-link :to="`/user/${userInfo.user}`">
+        {{ userInfo.id }}
+      </router-link> -->
+      <div class="time">
+        {{ userInfo.created }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
+  },
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
