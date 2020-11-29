45381c8e5d09e450c174758aad7c5308cc499e88
HwangSunTae <pozafly@gmail.com>
2020-11-29 21:07:26 +0900

    actions에 return문 넣기. return없을경우 비동기불가

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,7 @@ import {
 
 export default {
   FETCH_NEWS({ commit }) {
-    fetchNewsList()
+    return fetchNewsList()
       .then(response => {
         commit('SET_NEWS', response.data);
         return response;
@@ -17,27 +17,27 @@ export default {
       .catch(e => console.log(e));
   },
   FETCH_ASK(context) {
-    fetchAskList()
+    return fetchAskList()
       .then(({ data }) => context.commit('SET_ASK', data))
       .catch(e => console.log(e));
   },
   FETCH_JOBS({ commit }) {
-    fetchJobsList()
+    return fetchJobsList()
       .then(({ data }) => commit('SET_JOBS', data))
       .catch(e => console.log(e));
   },
   FETCH_USER({ commit }, name) {
-    fetchUserInfo(name)
+    return fetchUserInfo(name)
       .then(({ data }) => commit('SET_USER', data))
       .catch(e => console.log(e));
   },
   FETCH_ITEM({ commit }, item) {
-    fetchItemInfo(item)
+    return fetchItemInfo(item)
       .then(({ data }) => commit('SET_ITEM', data))
       .catch(e => console.log(e));
   },
   FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName)
+    return fetchList(pageName)
       .then(({ data }) => commit('SET_LIST', data))
       .catch(e => console.log(e));
   }
